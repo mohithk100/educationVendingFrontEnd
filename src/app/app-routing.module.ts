44f056fe4301717f69e52be39987f5ef14222a58
Mohith Khatri <mohithkhatri100@gmail.com>
2018-10-13 15:43:43 +0530

    created login-modal and registration-modal components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+import { RegistrationModalComponent } from './registration-modal/registration-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login' , component : LoginModalComponent },
+  { path: 'register' , component: RegistrationModalComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [
+    LoginModalComponent,
+    RegistrationModalComponent
+  ]
